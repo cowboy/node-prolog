2958ff1e044747c2206a17154359a315d2a62885
Ben Alman <cowboy@rj3.net>
2013-07-23 11:34:12 -0400

    Allow a Muxer formatString. Also, code cleanup.

--- muxer_example.js
+++ muxer_example.js
@@ -1,14 +1,14 @@
 var Muxer = require('./lib/muxer').Muxer;
 var Progress = require('./lib/progress').Progress;
 
-var muxer = new Muxer();
+var muxer = new Muxer('[muxer] %s');
 muxer.stream.pipe(process.stdout);
 
-muxer.write('[foo] This is a test.');
-muxer.write('[foo] Testing %s: %d, %j.', 'A', 123, {a: 1});
+muxer.write('This is a test.');
+muxer.write('Testing %s: %d, %j.', 'A', 123, {a: 1});
 
 var progress = new Progress('Progress... ', {
-  logger: muxer.write.bind(muxer, '[foo] %s')
+  logger: muxer.write.bind(muxer, '%s')
 });
 
 var through = require('through');
@@ -29,17 +29,17 @@ var id = setInterval(function() {
 }, 35);
 
 var counter = 0;
-var max = 10;
+var max = 100;
 (function loopy() {
   counter++;
-  progress.update(counter, max);
+  progress.update(max - counter + 1, max);
   if (counter < max) {
-    setTimeout(loopy, 100);
+    setTimeout(loopy, 20);
   } else {
     // console.log('[con] Testing %s: %d, %j.', 'B', 456, {a: 2});
-    muxer.write('[foo] Testing %s: %d, %j.', 'B', 456, {a: 2});
+    muxer.write('Testing %s: %d, %j.', 'B', 456, {a: 2});
     progress.done('OK');
-    muxer.write('[foo] Testing %s: %d, %j.', 'C', 789, {a: 3});
+    muxer.write('Testing %s: %d, %j.', 'C', 789, {a: 3});
     done = true;
   }
 }());
