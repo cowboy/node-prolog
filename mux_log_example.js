fd4b632205494c9e949223b84cbf8547e1b16a77
Ben Alman <cowboy@rj3.net>
2013-07-25 12:21:09 -0400

    Combining… works, but doesn't. Need a new approach.

--- mux_log_example.js
+++ mux_log_example.js
@@ -2,6 +2,8 @@
 
 var colors = require('colors');
 
+var _ = require('lodash');
+
 var log = {};
 
 log.Muxer = require('./lib/muxer').Muxer;
@@ -29,8 +31,18 @@ log.addLevel = function(name, formatString) {
 
 var through = require('through');
 log.combine = function() {
+  var levels = _(arguments).toArray().groupBy(function(s) {
+    return /^!/.test(s) ? 'exclude' : 'include';
+  }).value();
+  if (!levels.include) {
+    levels.include = Object.keys(log.levels);
+  }
+  if (levels.exclude) {
+    levels.exclude = levels.exclude.map(function(s) { return s.slice(1); });
+    levels.include = _.difference(levels.include, levels.exclude);
+  }
   var stream = through();
-  [].slice.apply(arguments).forEach(function(level) {
+  levels.include.forEach(function(level) {
     log[level].stream.pipe(stream);
   });
   return stream;
@@ -38,19 +50,22 @@ log.combine = function() {
 
 // add logging level methods / streams
 
-log.addLevel('info', '[INFO] %s'.green);
+log.addLevel('log', '[LOG] %s');
+log.addLevel('info', '[INFO] %s'.cyan);
+log.addLevel('debug', '[DEBUG] %s'.magenta);
 log.addLevel('warn', '[WARN] %s'.yellow);
 log.addLevel('error', '[ERROR] %s'.red);
 
 // set up streams
 
-log.combine('info', 'warn').pipe(process.stdout);
-log.error.stream.pipe(process.stderr);
+log.combine('log', 'info').pipe(process.stdout);
+log.combine('warn', 'error').pipe(process.stderr);
+// log.debug.stream.pipe(process.stdout);
 
-log.combine('info', 'warn', 'error')
+log.combine()//'!debug')
   .pipe(through(function(data) {
     data = colors.stripColors(data);
-    data = data.replace(/\r/g, '\n');
+    data = data.replace(/\r/g, '\n•');
     this.queue(data);
   }))
   .pipe(require('fs').createWriteStream('tmp/out2.txt'));
@@ -58,10 +73,14 @@ log.combine('info', 'warn', 'error')
 // simulation
 
 var cmds = [
-  log.info.bind(log, 'This is a test info.'),
-  log.warn.bind(log, 'This is a test warning.'),
-  log.error.bind(log, 'This is a test error.'),
+  log.log.bind(log, 'This is a test log message.'),
+  log.info.bind(log, 'This is a test info message.'),
+  log.debug.bind(log, 'This is a test debug message.'),
+  log.warn.bind(log, 'This is a test warning message.'),
+  log.error.bind(log, 'This is a test error message.'),
+  log.log.bind(log, 'Testing log %s: %d, %j.', 'A', 123, {a: 1}),
   log.info.bind(log, 'Testing info %s: %d, %j.', 'A', 123, {a: 1}),
+  log.debug.bind(log, 'Testing debug %s: %d, %j.', 'A', 123, {a: 1}),
   log.warn.bind(log, 'Testing warning %s: %d, %j.', 'A', 123, {a: 1}),
   log.error.bind(log, 'Testing error %s: %d, %j.', 'A', 123, {a: 1}),
 ];
@@ -75,7 +94,7 @@ var id1 = setInterval(function() {
   }
 }, 200);
 
-var p = log.info.progress('Progress...');
+var p = log.log.progress('Progress...');
 var max = 11;
 var counter = 0;
 var id2 = setInterval(function() {
