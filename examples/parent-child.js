9b44dc2340e2613cd04cc1357b6e7889eb695700
Ben Alman <cowboy@rj3.net>
2013-08-31 09:29:24 -0400

    Added functions for child debugging output.

--- examples/parent-child.js
+++ examples/parent-child.js
@@ -49,25 +49,59 @@ var childLog = new ProLog(parentLog, {
 
 console.log('===== Parent and Child Logs =====\n');
 
-parentLog.group('Logging levels that don\'t exist on a logger can\'t be called.');
-parentLog.log('This log message comes from the parent.');
-parentLog.parentonly('This "parentonly" message comes from the parent.');
-try {
-  parentLog.childonly('This will throw an exception.');
-} catch (err) {
-  parentLog.error('Exception: %s', err.message);
+function parentExample() {
+  parentLog.group('Logging levels that don\'t exist on a logger can\'t be called.');
+  parentLog.log('This log message comes from the parent.');
+  parentLog.parentonly('This "parentonly" message comes from the parent.');
+  try {
+    parentLog.childonly('This will throw an exception.');
+  } catch (err) {
+    parentLog.error('Exception: %s', err.message);
+  }
+  parentLog.groupEnd();
 }
-parentLog.groupEnd();
 
-childLog.group('But will be passed-through.');
-childLog.log('This log message comes from the child.');
-childLog.childonly('This "childonly" message comes from the child.');
-try {
-  childLog.parentonly('This will throw an exception.');
-} catch (err) {
-  childLog.error('Exception: %s', err.message);
+function childExample() {
+  childLog.group('But will be passed-through.');
+  childLog.log('This log message comes from the child.');
+  childLog.childonly('This "childonly" message comes from the child.');
+  try {
+    childLog.parentonly('This will throw an exception.');
+  } catch (err) {
+    childLog.error('Exception: %s', err.message);
+  }
+  childLog.groupEnd();
 }
-childLog.groupEnd();
+
+function groupingExample() {
+  parentLog.header('Note that indentation is cumulative.');
+  parentLog.log('This parent log message should not be indented.');
+  childLog.log('This child log message should not be indented.');
+  parentLog.group('[1] Increase parentLog indent');
+  parentLog.log('This parent log message should be indented once.');
+  childLog.log('This child log message should be indented once.');
+  childLog.group('[2] Increase childLog indent');
+  parentLog.log('This parent log message should still be indented once.');
+  childLog.log('This child log message should be indented twice.');
+  childLog.log([['This array will be indented twice'], ['and logged over'], ['multiple lines.']]);
+  childLog.log('Testing twice-indented child log message %s: %d, %j.', 'A', 1, {a: 1});
+  childLog.groupEnd();
+  childLog.header('[2] Decrease childLog indent');
+  parentLog.log('This parent log message should still be indented once.');
+  childLog.log('This child log message should be indented once.');
+  parentLog.groupEnd();
+  parentLog.header('[1] Decrease parentLog indent');
+  parentLog.log('This parent log message should not be indented.');
+  childLog.log('This child log message should not be indented.');
+  childLog.group('[3] Increase childLog indent');
+  parentLog.log('This parent log message should not be indented.');
+  childLog.log('This child log message should be indented once.');
+  childLog.groupEnd();
+  childLog.header('[3] Decrease childLog indent');
+}
+
+parentExample();
+childExample();
 
 // Help differentiate childLog messages visually.
 childLog.filter = function(data) {
@@ -75,30 +109,7 @@ childLog.filter = function(data) {
 };
 childLog.log('All childlog messages should now be yellow.');
 
-parentLog.header('Note that indentation is cumulative.');
-parentLog.log('This parent log message should not be indented.');
-childLog.log('This child log message should not be indented.');
-parentLog.group('[1] Increase parentLog indent');
-parentLog.log('This parent log message should be indented once.');
-childLog.log('This child log message should be indented once.');
-childLog.group('[2] Increase childLog indent');
-parentLog.log('This parent log message should still be indented once.');
-childLog.log('This child log message should be indented twice.');
-childLog.log([['This array will be indented twice'], ['and logged over'], ['multiple lines.']]);
-childLog.log('Testing twice-indented child log message %s: %d, %j.', 'A', 1, {a: 1});
-childLog.groupEnd();
-childLog.header('[2] Decrease childLog indent');
-parentLog.log('This parent log message should still be indented once.');
-childLog.log('This child log message should be indented once.');
-parentLog.groupEnd();
-parentLog.header('[1] Decrease parentLog indent');
-parentLog.log('This parent log message should not be indented.');
-childLog.log('This child log message should not be indented.');
-childLog.group('[3] Increase childLog indent');
-parentLog.log('This parent log message should not be indented.');
-childLog.log('This child log message should be indented once.');
-childLog.groupEnd();
-childLog.header('[3] Decrease childLog indent');
+groupingExample();
 
 console.log('\n===== Just Child Logs =====\n');
 console.log(childLogs.join('\n'));
